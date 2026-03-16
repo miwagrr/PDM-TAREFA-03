a611e6fe81e1e23f67c89d09d408b62f2940ae77
mar 🌺 <99049408+miwagrr@users.noreply.github.com>
2026-03-15 22:32:14 -0300

    adicionando filmeViewModel

--- Exercicio 01/viewModel/filmeViewModel.js
+++ Exercicio 01/viewModel/filmeViewModel.js
@@ -1,1 +1,29 @@
+import React, { useState } from 'react';
+import { FilmeBusiness } from '../business/filmeBusiness';
 
+const business = new FilmeBusiness();
+
+export function useFilmesViewModel() {
+  const [filmes, setFilmes] = useState(business.listar());
+  const [erro, setErro] = useState(null);
+  const [tituloInput, setTituloInput] = useState("");
+
+  function adicionarFilme() {
+    try {
+      business.addFilme(tituloInput, filmes);
+      setFilmes(business.listar());
+      setErro(null);
+      setTituloInput("");
+    } catch (e) {
+      setErro(e.message);
+    }
+  }
+
+  return {
+    filmes,
+    erro,
+    tituloInput,
+    setTituloInput,
+    adicionarFilme,
+  };
+}
