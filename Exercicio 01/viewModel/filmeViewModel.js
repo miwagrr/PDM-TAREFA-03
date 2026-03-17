d76be14c2d821e0831edbe9181052bf5bb415e2a
mar 🌺 <99049408+miwagrr@users.noreply.github.com>
2026-03-17 20:43:19 -0300

    atualizando filmeViewModel.js

--- Exercicio 01/viewModel/filmeViewModel.js
+++ Exercicio 01/viewModel/filmeViewModel.js
@@ -3,7 +3,7 @@ import { FilmeBusiness } from '../business/filmeBusiness';
 
 const business = new FilmeBusiness();
 
-export function useFilmesViewModel() {
+export function UseFilmesViewModel() {
   const [filmes, setFilmes] = useState(business.listar());
   const [erro, setErro] = useState(null);
   const [tituloInput, setTituloInput] = useState("");
@@ -19,11 +19,17 @@ export function useFilmesViewModel() {
     }
   }
 
+  function removerFilme(id) {
+    business.removerFilme(id);
+    setFilmes(business.listar());
+  }
+
   return {
     filmes,
     erro,
     tituloInput,
     setTituloInput,
     adicionarFilme,
+    removerFilme,
   };
 }
