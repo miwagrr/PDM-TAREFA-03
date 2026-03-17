77f4f3c685cddc5151b53da91cb260924e889657
mar 🌺 <99049408+miwagrr@users.noreply.github.com>
2026-03-17 20:42:37 -0300

    adicionando funcao removerfilme

--- Exercicio 01/business/filmeBusiness.js
+++ Exercicio 01/business/filmeBusiness.js
@@ -1,35 +1,40 @@
 import { Filme } from '../models/filmeModel';
 
-let filmes: Filme[] = []; 
+let filmes: Filme[] = [];
 let proxId = 1;
 
 export class FilmeBusiness {
-  
   listar(): Filme[] {
     return [...filmes];
   }
 
   addFilme(titulo: string, lista: Filme[]): Filme | null {
     if (!titulo) {
-      throw new Error("Nome é obrigatório");
-    }
-    
-    if (titulo.length <= 2) {
-      throw new Error("O título deve ter mais de 2 caracteres");
+      throw new Error('Nome é obrigatório');
     }
 
-    const jaExiste = lista.find(f => f.titulo.toLowerCase() === titulo.toLowerCase());
+    if (titulo.length <= 2) {
+      throw new Error('O título deve ter mais de 2 caracteres');
+    }
+
+    const jaExiste = lista.find(
+      (f) => f.titulo.toLowerCase() === titulo.toLowerCase()
+    );
 
     if (jaExiste) {
-      throw new Error("O filme já está em sua WatchList");
+      throw new Error('O filme já está em sua WatchList');
     }
 
-    const novo: Filme = { 
-      id: proxId++, 
-      titulo: titulo 
+    const novo: Filme = {
+      id: proxId++,
+      titulo: titulo,
     };
 
     filmes.push(novo);
-    return novo; 
+    return novo;
+  }
+
+  removerFilme(id){
+    filmes = filmes.filter(f => f.id !== id)
   }
 }
