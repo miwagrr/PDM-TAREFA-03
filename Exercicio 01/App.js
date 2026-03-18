25bbcef42f85a6e1d056f663e9c0ffb32f5b8f72
mar 🌺 <99049408+miwagrr@users.noreply.github.com>
2026-03-17 21:45:18 -0300

    mudando img background

--- Exercicio 01/App.js
+++ Exercicio 01/App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <SafeAreaProvider>
       <ImageBackground 
-        source={{uri: 'https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTEwL3Jhd3BpeGVsb2ZmaWNlM19yb3dzX29mX2VtcHR5X3JlZF9zZWF0c19pbl9jaW5lbWFfZWVhNDA4NGItMTEwNC00MjY2LWE0NjctOTlhZDI5MTRkODQ5LWIuanBn.jpg'}}
+        source={{uri: 'https://4kwallpapers.com/images/wallpapers/dune-2-2024-movies-1242x2208-13967.jpg'}}
         style={{ flex: 1 }}
       >
         <SafeAreaView style={{ flex: 1 }}>
